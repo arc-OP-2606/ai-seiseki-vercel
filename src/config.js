e9a59c707712563e74e8006d4f8e9816aadcc6b2
arc-OP-2606 <ai.apparc@gmail.com>
2026-06-20 21:26:44 +0900

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const GAS_API_URL = "https://script.google.com/macros/s/AKfycbzEOaN_RVtktQICMCxJVz2wjEQWJTLYvAgUWmTiofIrKw-0_xVr08Tr_wXjMrERYiv0JQ/exec";
+export const GAS_API_URL = "/api/gas";
